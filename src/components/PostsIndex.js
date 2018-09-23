3ee040122106e2167d48ef74cab5a9455cb5879d
veselinn16 <veselinn16@gmail.com>
2018-09-23 16:37:55 +0300

    Get request sent on page load :wrench:

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getPosts } from '../actions/index';
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.getPosts();
+    }
+
     render() {
         return (
             <div>Posts Index</div>
@@ -8,4 +14,4 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+export default connect(null, { getPosts })(PostsIndex); // {getPosts: getPosts}
