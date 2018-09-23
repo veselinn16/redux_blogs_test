3561d2eb39c825ebb177081fd49984aa6ed50b94
veselinn16 <veselinn16@gmail.com>
2018-09-23 15:12:24 +0300

    Construct GET request for posts API :wrench:

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
+import axios from 'axios';
+
 export const GET_POSTS = 'GET_POSTS'
 
+const rootUrl = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=redux032'
+
 export function getPosts() {
+    const request = axios.get(`${rootUrl}/posts${API_KEY}`);
+
     return {
-        type: GET_POSTS
+        type: GET_POSTS,
+        payload: request
     }
 }
