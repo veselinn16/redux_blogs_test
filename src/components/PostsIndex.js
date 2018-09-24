aa9759252d933c37a23a651272973da5c9f87e3a
veselinn16 <veselinn16@gmail.com>
2018-09-24 14:32:00 +0300

    Render post on page :sparkles:

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getPosts } from '../actions/index';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
     componentDidMount() {
         this.props.getPosts();
     }
 
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return <li className="list-group-item" key={post.id}>
+                {(!post.title) ? 'kus' : post.title}
+            </li>
+        });
+    }
+
     render() {
         return (
-            <div>Posts Index</div>
+            <div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
         )
     }
 }
 
-export default connect(null, { getPosts })(PostsIndex); // {getPosts: getPosts}
+function mapStateToProps(state) {
+    return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { getPosts })(PostsIndex); // {getPosts: getPosts}
