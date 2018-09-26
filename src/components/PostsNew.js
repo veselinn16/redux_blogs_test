564232da2876987854b9bdeb65da1e4a036b2730
veselinn16 <veselinn16@gmail.com>
2018-09-26 19:40:41 +0300

    Setup validation for new Post component :wrench:

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -11,21 +11,28 @@ class PostsNew extends Component {
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
                 <Field
                     label="Title"
                     name="title"
                     component={this.renderField} 
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField} 
                 />
                 <Field
@@ -33,11 +40,32 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField} 
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
     }
 }
 
+function validate(values) {
+    // values -> { title: 'asf', categories: 'fsdf', content: 'fdsf'}
+    const errors = {};
+
+    // validate inputs from values
+    if(!values.title || values.title.length < 3) {
+        errors.title = 'Enter a title that is ate least 3 characters, please!'
+    }
+    if(!values.categories) {
+        errors.categories = 'Enter categories, please!'
+    }
+    if(!values.content) {
+        errors.content = 'Enter content, please!'
+    }
+
+    // if errors is an empty object, there is no issue with form submission!
+    return errors;
+}
+
 export default reduxForm({
-    form: 'PostsNewForm'
+    validate,
+    form: 'PostsNewForm',
 })(PostsNew);
