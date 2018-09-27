69590dc1097a63716ca4fa7dc966cba7d9d9ced6
veselinn16 <veselinn16@gmail.com>
2018-09-27 11:44:19 +0300

    Add new post action creator :wrench:

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const GET_POSTS = 'GET_POSTS'
+export const GET_POSTS = 'GET_POSTS';
+export const CREATE_POSTS = 'CREATE_POSTS';
 
 const rootUrl = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=redux032'
@@ -10,6 +11,15 @@ export function getPosts() {
 
     return {
         type: GET_POSTS,
+        payload: request
+    }
+}
+
+export function createPost(values) {
+    const request = axios.post(`${rootUrl}/posts${API_KEY}`, values);
+
+    return {
+        type: CREATE_POSTS,
         payload: request
     }
 }
