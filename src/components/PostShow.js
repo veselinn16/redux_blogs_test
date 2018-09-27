913ad41af435d36f6013efedbd0e2db75cc59588
veselinn16 <veselinn16@gmail.com>
2018-09-27 21:51:05 +0300

    Render button for index in post page :sparkles:

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getPost } from '../actions/index';
 
@@ -14,9 +15,10 @@ class PostShow extends Component {
         if(!post) {
             return <div>Loading...</div>
         }
-        
+
         return (
             <div>
+                <Link to='/' className="btn btn-primary">Back To Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
