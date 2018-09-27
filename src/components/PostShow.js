bbe8df25878be28e9daa33d62ba8411c2e785a0a
veselinn16 <veselinn16@gmail.com>
2018-09-27 20:34:45 +0300

    Extract post from state in PostShow component :wrench:

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getPost } from '../actions/index';
 
 class PostShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params.id; // provided to us by React-router
+        this.props.getPost(id);
+    }
+
     render() {
         return (
             <div>
@@ -10,4 +17,8 @@ class PostShow extends Component {
     }
 }
 
-export default PostShow;
+function mapStateToProps({ posts } , ownProps) { // state.posts
+    return { post: posts[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { getPost })(PostShow);
