30bfc6544adbc958b24eb3c2ab8de7b4317023b5
veselinn16 <veselinn16@gmail.com>
2018-09-27 12:57:32 +0300

    Create PostShow component :construction:

---
+++ src/components/PostShow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostShow extends Component {
+    render() {
+        return (
+            <div>
+                Post Show
+            </div>
+        )
+    }
+}
+
+export default PostShow;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/PostsIndex';
 import PostsNew from './components/PostsNew';
+import PostShow from './components/PostShow';
 
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -16,8 +17,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route exact path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostShow} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
